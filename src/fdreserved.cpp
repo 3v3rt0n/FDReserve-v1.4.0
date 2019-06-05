a7573ddcbc099ed52b36a6f6fe4e412eb7a34da3
3v3rt0n <42306580+3v3rt0n@users.noreply.github.com>
2019-06-05 14:50:56 -0300

    fix

--- src/fdreserved.cpp
+++ src/fdreserved.cpp
@@ -70,13 +70,13 @@ bool AppInit(int argc, char* argv[])
 
     // Process help and version before taking care about datadir
     if (mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version")) {
-        std::string strUsage = _("FDReserve Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";
+        std::string strUsage = _("FD Reserve Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";
 
         if (mapArgs.count("-version")) {
             strUsage += LicenseInfo();
         } else {
             strUsage += "\n" + _("Usage:") + "\n" +
-                        "  fdreserved [options]                     " + _("Start FDReserve Core Daemon") + "\n";
+                        "  fdreserved [options]                     " + _("Start FD Reserve Core Daemon") + "\n";
 
             strUsage += "\n" + HelpMessage(HMM_BITCOIND);
         }
@@ -122,7 +122,7 @@ bool AppInit(int argc, char* argv[])
 #ifndef WIN32
         fDaemon = GetBoolArg("-daemon", false);
         if (fDaemon) {
-            fprintf(stdout, "FDReserve server starting\n");
+            fprintf(stdout, "FD Reserve server starting\n");
 
             // Daemonize
             pid_t pid = fork();
